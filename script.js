fd15c75da5c30e6288047d7eb3ff9136bbcf4ed8
maLu70 <lumasiqueira06@gmail.com>
2024-11-29 21:48:20 -0300

    troca de radios e perguntas com array certo

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-let arrayPerguntas = ["a", "b", "c"]
+let arrayPerguntas = [
+    per1 = {
+        pergun: "test1",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d"
+    },
+    per2 = {
+        pergun: "test2",
+        a: "aa",
+        b: "bb",
+        c: "cc",
+        d: "dd"
+    }
+]
+
+let arrayRespostas = []
+
 let i = 0
 
 function comecar() {
     document.querySelector(".pgPerguntas").style.display = ("block")
     document.querySelector(".pgInicial").style.display = ("none")
     
-    document.querySelector("#perguntas").innerHTML = arrayPerguntas[0]
+    document.querySelector("#perguntas").innerHTML = arrayPerguntas[0].pergun
 
+    alternativas()
 }
 
 function proxima() {
@@ -24,8 +43,10 @@ function anterior() {
 function controlePerguntas() {
     let btnAnterior = document.querySelector("#btnAnt")
     let btnFinal = document.querySelector("#btnFinal")
-    document.querySelector("#perguntas").innerHTML = arrayPerguntas[i]
+    document.querySelector("#perguntas").innerHTML = arrayPerguntas[i].pergun
     let btnProximo = document.getElementById("btnProxi")
+
+    alternativas()
 
     if (i > 0) {
         btnAnterior.style.display = ("block")
@@ -35,10 +56,10 @@ function controlePerguntas() {
 
     if (i == arrayPerguntas.length - 1) {
         btnFinal.style.display = ("block")
-        btnProximo.style.display = "none"
+        // btnProximo.style.display = "none"
         console.log("foi")
     } else {
-        // btnFinal.style.display = "block"
+        btnFinal.style.display = "none"
         // btnProximo.style.display = "none"
     }
 
@@ -47,4 +68,13 @@ function controlePerguntas() {
     } else {
         console.log("nãoachie")
     }
+}
+
+function alternativas() {
+    let lbls = document.querySelectorAll('label')
+
+    lbls[0].innerHTML = arrayPerguntas[i].a
+    lbls[1].innerHTML = arrayPerguntas[i].b
+    lbls[2].innerHTML = arrayPerguntas[i].c
+    lbls[3].innerHTML = arrayPerguntas[i].d
 }
